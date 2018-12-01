fb06ff02adeff9027c800c522de0a341a13924c0
Søren Fuglede Jørgensen <s@fuglede.dk>
2018-12-01 08:20:03 +0100

    Make solution slightly more memory efficient

--- day01/solutions.py
+++ day01/solutions.py
@@ -1,5 +1,3 @@
-import itertools
-
 import numpy as np
 import pandas as pd
 
@@ -10,25 +8,15 @@ print(df.sum()[0])
 
 
 # Part two
-def find_first_duplicate(a):
-    # If we were to just go through the list of changes once,
-    # we would just be interested in its cumulative sum. We
-    # then note the following relationship on subsequent passes
-    # through the list of changes:
-    #   pass0 = cumsum
-    #   pass1 = pass0 + pass0[-1] = cumsum + cumsum[-1]
-    #   pass2 = pass1 + pass1[-1] = cumsum + (cumsum + cumsum[-1])[-1]
-    #                             = cumsum + 2*cumsum[-1]
-    #   ...
-    # and in general, by induction, the i'th pass is just cumsum + i*cumsum[-1].
-    cs = np.cumsum(a)
+def find_first_duplicate(changes):
+    freqs = np.cumsum(changes)
     seen = set()
-    for i in itertools.count():
-        freqs = cs + i*cs[-1]
+    while True:
         for b in freqs:
             if b in seen:
                 return b
             seen.add(b)
+        freqs += changes[0] + freqs[-1] - freqs[0]
 
 
 print(find_first_duplicate(df.values))
