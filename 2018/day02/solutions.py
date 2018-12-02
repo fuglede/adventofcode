02d89c3cfb69876008ba74c1b19d457aacfbd71d
Søren Fuglede Jørgensen <s@fuglede.dk>
2018-12-02 07:44:27 +0100

    Use itertools for its wonderful expressiveness

--- 2018/day02/solutions.py
+++ 2018/day02/solutions.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from itertools import combinations
 
 with open('input') as f:
     words = [x.strip() for x in f.readlines()]
@@ -9,7 +10,6 @@ threes = [3 in Counter(word).values() for word in words]
 print(sum(twos)*sum(threes))
 
 # Part two
-for w1 in words:
-    for w2 in words:
-        if sum(l1 == l2 for l1, l2 in zip(w1, w2)) == len(w1) - 1:
-            print(''.join([l1 for l1, l2 in zip(w1, w2) if l1 == l2]))
+for w1, w2 in combinations(words, 2):
+    if sum(l1 == l2 for l1, l2 in zip(w1, w2)) == len(w1) - 1:
+        print(''.join([l1 for l1, l2 in zip(w1, w2) if l1 == l2]))
