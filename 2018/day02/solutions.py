daf189aaccd218cc36d9841c9e1d9fb27ea04623
Søren Fuglede Jørgensen <s@fuglede.dk>
2018-12-02 07:45:32 +0100

    Get rid of a few redundant lists; nobody likes redundant lists

--- 2018/day02/solutions.py
+++ 2018/day02/solutions.py
@@ -5,11 +5,11 @@ with open('input') as f:
     words = [x.strip() for x in f.readlines()]
 
 # Part one
-twos = [2 in Counter(word).values() for word in words]
-threes = [3 in Counter(word).values() for word in words]
+twos = (2 in Counter(word).values() for word in words)
+threes = (3 in Counter(word).values() for word in words)
 print(sum(twos)*sum(threes))
 
 # Part two
 for w1, w2 in combinations(words, 2):
     if sum(l1 == l2 for l1, l2 in zip(w1, w2)) == len(w1) - 1:
-        print(''.join([l1 for l1, l2 in zip(w1, w2) if l1 == l2]))
+        print(''.join(l1 for l1, l2 in zip(w1, w2) if l1 == l2))
