89533863d5d1e466a0fb5a2996474dcdeb5bf2a1
Søren Fuglede Jørgensen <s@fuglede.dk>
2018-12-30 19:34:46 +0100

    Add solution to 2018-12-20

---
+++ 2018/day20/solutions.py
@@ -0,0 +1,40 @@
+import networkx as nx
+
+
+with open('input') as f:
+    data = f.read().strip()
+
+dirs = {'N': 1j, 'S': -1j, 'E': 1, 'W': -1}
+
+
+def parse(current, data, g, room_stack, end_stack, already_done):
+    if len(data) in already_done:
+        return
+    already_done.add(len(data))
+    for i, s in enumerate(data):
+        new = None
+        if s in 'NSEW':
+            g.add_edge(current, current + dirs[s])
+            current += dirs[s]
+        elif s == '(':
+            room_stack.append(current)
+            end_stack.append([])
+        elif s == '|' or s == ')':
+            end_stack[-1].append(current)
+            current = room_stack[-1]
+            if s == ')':
+                room_stack.pop()
+                ends = end_stack.pop()
+                for end in set(ends):
+                    parse(end, data[i+1:], g, room_stack, end_stack, already_done)
+                return
+
+
+g = nx.Graph()
+parse(0, data, g, [], [], set())
+
+# Part one
+print(max(nx.shortest_path_length(g, 0).values()))
+
+# Part two
+print(sum(length >= 1000 for length in nx.shortest_path_length(g, 0).values()))
