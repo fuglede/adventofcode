2eb49d12324270fd7df81ded94b5d796052d83d1
Søren Fuglede Jørgensen <github@fuglede.dk>
2021-12-20 12:05:12 +0100

    Simplify solution to 2021-12-20

--- 2021/day20/solutions.py
+++ 2021/day20/solutions.py
@@ -1,37 +1,14 @@
-from collections import defaultdict
+import numpy as np
+from scipy.signal import convolve2d
 
 with open("input") as f:
-    data = f.read().strip()
+    ls = f.read().strip().split("\n")
 
-top, bottom = data.split("\n\n")
-enhancement = [x == "#" for x in top]
-inp = defaultdict(
-    bool,
-    {
-        i + 1j * j: x == "#"
-        for i, l in enumerate(bottom.split("\n"))
-        for j, x in enumerate(l)
-    },
-)
+enhancement = np.array(list(ls[0])) == "#"
+grid = np.array(list(map(list, ls[2:]))) == "#"
+kernel = 2 ** np.arange(9).reshape((3, 3))
 
-dirs = (1 + 1j, 1, 1 - 1j, 1j, 0, -1j, -1 + 1j, -1, -1 - 1j)
-
-
-def step(grid, value_at_infinity):
-    z1, *_, z2 = grid
-    out = defaultdict(lambda: value_at_infinity)
-    for x in range(int(z1.real) - 1, int(z2.real) + 2):
-        for y in range(int(z1.imag) - 1, int(z2.imag) + 2):
-            z = x + 1j * y
-            head = sum(2 ** i * grid[z + dz] for i, dz in enumerate(dirs))
-            out[z] = enhancement[head]
-    return out
-
-
-# Part one
-print(sum(step(step(inp, True), False).values()))
-
-# Part two
 for i in range(50):
-    inp = step(inp, not i % 2)
-print(sum(inp.values()))
+    grid = enhancement[convolve2d(grid, kernel, fillvalue=i % 2)]
+    if i in (1, 49):
+        print(grid.sum())
