58d255b5cb4d47b074fc7fc6e46a7af5ca4549a7
Søren Fuglede Jørgensen <github@fuglede.dk>
2022-12-04 06:16:13 +0100

    Add solution to 2022-12-04

---
+++ 2022/day04/solutions.py
@@ -0,0 +1,13 @@
+import re
+
+with open("input") as f:
+    ls = f.readlines()
+
+ns = [list(map(int, re.findall("\d+", x))) for x in ls]
+ranges = [(set(range(n1, n2 + 1)), set(range(n3, n4 + 1))) for n1, n2, n3, n4 in ns]
+
+# Part 1
+print(sum(s1 <= s2 or s2 <= s1 for s1, s2 in ranges))
+
+# Part 2
+print(sum(bool(s1 & s2) for s1, s2 in ranges))
