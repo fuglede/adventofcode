32ec1500cc89dffe0ae5892e41e7cf34262037cb
Søren Fuglede Jørgensen <github@fuglede.dk>
2023-12-01 06:20:50 +0100

    Add solution to 2023-12-01

---
+++ 2023/day01/solutions.py
@@ -0,0 +1,28 @@
+import re
+
+with open("input") as f:
+    data = f.read().strip()
+
+
+def calibration(data):
+    ls = data.split("\n")
+    ns = [list(map(int, re.findall("\d", x))) for x in ls]
+    return sum(10 * n[0] + n[-1] for n in ns)
+
+
+# Part 1
+print(calibration(data))
+
+# Part 2
+data = (
+    data.replace("one", "one1one")
+    .replace("two", "two2two")
+    .replace("three", "three3three")
+    .replace("four", "four4four")
+    .replace("five", "five5five")
+    .replace("six", "six6six")
+    .replace("seven", "seven7seven")
+    .replace("eight", "eight8eight")
+    .replace("nine", "nine9nine")
+)
+print(calibration(data))
